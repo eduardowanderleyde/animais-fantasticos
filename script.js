cd1206bd08e8cafe31e4642c30e22c31551c59d9
Eduardo Wanderley de Siqueira Araújo <eduardo.2020250145@unicap.br>
2023-10-27 19:36:32 -0300

    BUILT - Scroll animation

--- script.js
+++ script.js
@@ -73,3 +73,25 @@ function initScrollSuave() {
 }
 
 initScrollSuave();
+
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll('.js-scroll');
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top - windowMetade;
+        const isSectionVisible = (sectionTop - windowMetade) < 0;
+        if(sectionTop < 0) 
+          section.classList.add('ativo');
+        else
+          section.classList.remove('ativo');
+      })
+    }
+    animaScroll();
+    window.addEventListener('scroll', animaScroll);
+  }
+}
+
+initAnimacaoScroll();
