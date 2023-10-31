09fba5aa33ffbd7d60ec51987846158ad19c3411
Eduardo Wanderley de Siqueira Araújo <eduardo.2020250145@unicap.br>
2023-10-31 13:31:28 -0300

    BUILT - Make the modal to the login button

---
+++ js/modules/modal.js
@@ -0,0 +1,19 @@
+export default function modal() {
+}
+
+const botaoAbrir = document.querySelector('[data-modal="abrir"]');
+const botaoFechar = document.querySelector('[data-modal="fechar"]');
+const containerModal = document.querySelector('[data-modal="container"]');
+
+function abrirModal(event) {
+  event.preventDefault();
+  containerModal.classList.add('ativo');
+}
+
+function fecharModal(event) {
+  event.preventDefault();
+  containerModal.classList.remove('ativo');
+}
+
+botaoAbrir.addEventListener('click', abrirModal);
+botaoFechar.addEventListener('click', fecharModal);
