708a92808e32a7081a6b5fd2c71a1f9b3b02795d
Eduardo Wanderley de Siqueira Araújo <eduardo.2020250145@unicap.br>
2023-11-02 16:11:28 -0300

    BUILT - Make a bitcoin fetch API

--- js/script.js
+++ js/script.js
@@ -6,9 +6,9 @@ import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
-
 import initFuncionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetch-animais.js';
+import initFetchBitcoin from './modules/fetch-bitcoin.js';
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -20,3 +20,4 @@ initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
 initFetchAnimais();
+initFetchBitcoin();
