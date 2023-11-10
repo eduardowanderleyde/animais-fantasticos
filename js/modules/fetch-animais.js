4c41711af01457ba03c8ce4c7306fe8d0e37f739
Eduardo Wanderley de Siqueira Araújo <eduardo.2020250145@unicap.br>
2023-11-10 14:37:35 -0300

    BUILT - Refactor Fecth Animals

--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -1,27 +1,41 @@
 import AnimaNumeros from './anima-numeros.js';
 
-export default function initFetchAnimais() {
-  async function fetchAnimais(url){
+export default function fetchAnimais(url, target) {
+
+
+  function createAnimal(animal){
+    const div = document.createElement('div');
+    div.classList.add('numero-animal');
+    div.innerHTML = `<h3>${animal.specie}</h3><span data-number>${animal.total}</span>`;
+   return div;
+  }
+
+  //Each animal using fetch
+  const numerosGrid = document.querySelector(target);
+  function preencherAnimais(animal) {
+    const divAnimal = createAnimal(animal);
+    numerosGrid.appendChild(divAnimal); 
+  }
+  
+  function animaAnimaisNumeros() {
+    const animaNumeros = new AnimaNumeros('[data-numero]','.numeros', 'ativo');
+    animaNumeros.init();
+  }
+
+  //Fetch animals and create each animal using createAnimal function
+  async function criarAnimais(){
     try {
+
+      //fetch and wait for response, JSON
       const animaisResponse = await fetch(url);
       const animaisJSON = await animaisResponse.json();
-      const numerosGrid = document.querySelector('.numeros-grid');
-      animaisJSON.forEach(animal => {
-        const divAnimal = createAnimal(animal);
-        numerosGrid.appendChild(divAnimal); 
-      });
-      const animaNumeros = new AnimaNumeros('[data-numero]','.numeros', 'ativo');
-      animaNumeros.init();
-    } catch(erro){
-  console.log(erro);
-  }
-}
-function createAnimal(animal){
-  const div=document.createElement('div');
-  div.classList.add('numero-animal');
-  div.innerHTML = `<h3>${animal.specie}</h3><span data-number>${animal.total}</span>`;
- return div;
-}
 
-fetchAnimais('./animaisapi.json');
+      //After convert to JSON, activate the function to fill and animate the numbers
+      animaisJSON.forEach(animal => preencherAnimais(animal));
+      animaAnimaisNumeros();
+    } catch(erro){
+      console.log(erro);
+    }
+}
+return criarAnimais();
 }
